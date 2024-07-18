fbf3f0612d0fdfdc3d692baea24d3700edda4803
Michael <141164270+Skelligg@users.noreply.github.com>
2024-07-18 11:48:47 +0100

    Mostly finished site

--- build/static/script.js
+++ build/static/script.js
@@ -8,69 +8,76 @@ gsap.registerPlugin(ScrollTrigger);
 
 init();
 
-
 function init(){
     hamburger.addEventListener("click",toggleSideBar)
-    gsap.from('.workPopUp', {scrollTrigger: {
-        trigger: "#home",
-        start: "top-=50 top",
-        end: "top top"
-    }, duration: 1, y: "-50%", opacity: 0, ease: 'bounce', delay: 3,
-        onComplete: () =>{
-            gsap.to(".workPopUp", {scrollTrigger: {
-                trigger: "#work", 
-                start: "top center", 
+    if (window.scrollY === 0) {
+        gsap.from('.workPopUp', {
+            duration: 1,
+            y: "-50%",
+            opacity: 0,
+            ease: 'bounce',
+            delay: 3,
+            onComplete: createScrollTriggerAnimation
+        });
+    } else {
+        createScrollTriggerAnimation();
+    }
+}
 
-                toggleActions: "play none none reverse",
-                },
-                duration: 1, ease: 'ease-in',opacity: 0 } )
-        }
-    })
-
+function createScrollTriggerAnimation() {
+    gsap.to(".workPopUp", {
+        scrollTrigger: {
+            trigger: "#work",
+            start: "top 70%",
+            toggleActions: "play none none reverse",
+        },
+        duration: 1,
+        ease: 'ease-in',
+        opacity: 0
+    });
 }
 
 function toggleSideBar(){
     if(sideBar.classList.contains("w-16")){
         for(let a of sideBarTabs){
-            a.style.opacity = "1";
-            a.className = "sidebar-tab"
+            a.className = "sidebar-tab transition-[visibility] ease-in-out duration-100"
         }
 
         for(let sideName of sideNames){
-            sideName.classList.remove("delay-500")
+            sideName.classList.remove("delay-[400ms]")
             sideName.classList.remove("opacity-100")
             sideName.classList.add("opacity-0")
         }
 
         sideBar.classList.remove("delay-200")
-        sideBarTabs[0].className += " transition ease-in-out duration-100 delay-100"
-        sideBarTabs[1].className += " transition ease-in-out duration-100 delay-150"
-        sideBarTabs[2].className += " transition ease-in-out duration-100 delay-200"
-        sideBarTabs[3].className += " transition ease-in-out duration-100 delay-300"
+        sideBarTabs[0].className += " delay-100"
+        sideBarTabs[1].className += " delay-150"
+        sideBarTabs[2].className += " delay-200"
 
         sideBar.classList.remove("w-16")
         sideBar.className += " w-[180px]"
-
+        sideBar.className += " grey-out"
     }
     else{
         for(let a of sideBarTabs){
-            a.style.opacity = "0";
-            a.className = "sidebar-tab"
+            a.className = "sidebar-tab invisible transition-[visibility] ease-in-out duration-100"
+
         }
 
         for(let sideName of sideNames){
             sideName.classList.remove("opacity-0")
             sideName.classList.add("opacity-100")
-            sideName.classList.add("delay-500")
+            sideName.classList.add("delay-[400ms]")
         }
 
-        sideBarTabs[0].className += " transition ease-in-out duration-100 delay-150"
-        sideBarTabs[1].className += " transition ease-in-out duration-100 delay-100"
-        sideBarTabs[2].className += " transition ease-in-out duration-100 delay-75"
-        sideBarTabs[3].className += " transition ease-in-out duration-100 "
+
+        sideBarTabs[0].className += " delay-100"
+        sideBarTabs[1].className += " delay-75"
+        sideBarTabs[2].className += " "
 
         sideBar.className += " delay-200"
         sideBar.classList.remove("w-[180px]")
+        sideBar.classList.remove("grey-out")
         sideBar.className += " w-16"
     }
 
